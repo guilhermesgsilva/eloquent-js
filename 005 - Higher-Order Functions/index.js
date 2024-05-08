e81b5b1aa99b08d9b1ad6d38dc10ceb011151eb9
Guilherme Silva <silva.gsg@gmail.com>
2024-05-08 09:11:45 +0100

    feat: Chapter 5 exercise 3

--- 005 - Higher-Order Functions/index.js
+++ 005 - Higher-Order Functions/index.js
@@ -49,7 +49,37 @@ function loop(start, test, update, body) {
 // Everything
 console.log("Everything:");
 
-/*  */
+/* function every(array, test) {
+  for (let item of array) {
+    if (!test(item)) {
+      return false;
+    }
+  }
+  return true;
+} */
+
+function every(array, test) {
+  return !array.some((item) => !test(item));
+}
+
+console.log(every([1, 3, 5], (n) => n < 10));
+// true
+console.log(every([2, 4, 16], (n) => n < 10));
+// false
+console.log(every([], (n) => n < 10));
+// true
+
+/* 
+function every(array, predicate) {
+  for (let element of array) {
+    if (!predicate(element)) return false;
+  }
+  return true;
+}
+
+function every2(array, predicate) {
+  return !array.some(element => !predicate(element));
+} */
 
 // Dominant writing direction
 console.log("Dominant writing direction:");
